bcc2c4ae64762452db2d267958ba59ab4ea3a917
glopez <genghis.lopez@gmail.com>
2021-12-27 18:52:39 -0500

    initial

---
+++ paint clone template/script.js
@@ -0,0 +1,66 @@
+const activeToolEl = document.getElementById('active-tool');
+const brushColorBtn = document.getElementById('brush-color');
+const brushIcon = document.getElementById('brush');
+const brushSize = document.getElementById('brush-size');
+const brushSlider = document.getElementById('brush-slider');
+const bucketColorBtn = document.getElementById('bucket-color');
+const eraser = document.getElementById('eraser');
+const clearCanvasBtn = document.getElementById('clear-canvas');
+const saveStorageBtn = document.getElementById('save-storage');
+const loadStorageBtn = document.getElementById('load-storage');
+const clearStorageBtn = document.getElementById('clear-storage');
+const downloadBtn = document.getElementById('download');
+const { body } = document;
+
+// Global Variables
+
+let currentSize = 10;
+let bucketColor = '#FFFFFF';
+let currentColor = '#A51DAB';
+
+
+
+function createCanvas() {
+  // canvas.width = ;
+  // canvas.height = ;
+  // context.fillStyle = ;
+  // context.fillRect();
+  body.appendChild(canvas);
+
+}
+
+
+// Get Mouse Position
+function getMousePosition(event) {
+  const boundaries = canvas.getBoundingClientRect();
+  return {
+    x: event.clientX - boundaries.left,
+    y: event.clientY - boundaries.top,
+  };
+}
+
+// Mouse Down
+canvas.addEventListener('mousedown', (event) => {
+  isMouseDown = true;
+  const currentPosition = getMousePosition(event);
+  console.log('mouse is clicked', currentPosition);
+
+});
+
+// Mouse Move
+canvas.addEventListener('mousemove', (event) => {
+  if (isMouseDown) {
+    const currentPosition = getMousePosition(event);
+    console.log('mouse is moving', currentPosition);
+
+  }
+});
+
+// Mouse Up
+canvas.addEventListener('mouseup', () => {
+  isMouseDown = false;
+  console.log('mouse is unclicked');
+});
+
+
+createCanvas();
